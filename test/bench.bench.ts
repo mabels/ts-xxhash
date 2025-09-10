7fd72aba8945afff27d8a1b1da26a4d74388deb0
Meno Abels <meno.abels@adviser.com>
2025-09-10 10:57:25 +0200

    chore: added bench

--- test/bench.bench.ts
+++ test/bench.bench.ts
@@ -4,11 +4,47 @@ import { lorem_1mb as input } from "./lorem_1mb.txt.js";
 
 describe("XXH Benchmark", () => {
   const seed = 0xabcd;
-  bench("XXH one step", () => {
-    XXH.h32(input, seed).toString(16);
-  });
+  bench(
+    "XXH32 one step",
+    () => {
+      XXH.h32(input, seed).toString(16);
+    },
+    {
+      iterations: 50, // Run exactly 1000 times
+      time: 5000, // OR run for 5000ms (whichever comes first)
+    },
+  );
 
-  bench("XXH incremental", () => {
-    XXH.h32(seed).update(input).digest().toString(16);
-  });
+  bench(
+    "XXH32 incremental",
+    () => {
+      XXH.h32(seed).update(input).digest().toString(16);
+    },
+    {
+      iterations: 50, // Run exactly 1000 times
+      time: 5000, // OR run for 5000ms (whichever comes first)
+    },
+  );
+
+  bench(
+    "XXH64 one step",
+    () => {
+      XXH.h64(input, seed).toString(16);
+    },
+    {
+      iterations: 50, // Run exactly 1000 times
+      time: 5000, // OR run for 5000ms (whichever comes first)
+    },
+  );
+
+  bench(
+    "XXH64 incremental",
+    () => {
+      XXH.h64(seed).update(input).digest().toString(16);
+    },
+    {
+      iterations: 50, // Run exactly 1000 times
+      time: 5000, // OR run for 5000ms (whichever comes first)
+    },
+  );
 });
